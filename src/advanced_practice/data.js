53b0e449c149579e7a0302349c8d732f37cea954
Jamal Ahmed <j8ahmed@gmail.com>
2021-02-07 19:07:19 -0500

    removed example files.

--- src/advanced_practice/data.js
+++
@@ -1,8 +0,0 @@
-export const data = [
-    {id: 1, name: 'Jamal', job:'Doctor', salary:100_000},
-    {id: 2, name: 'Yusuf', job:'Engineer', salary:50_000},
-    {id: 3, name: 'Adam', job:'Trainer', salary:30_000},
-    {id: 4, name: 'David', job:'Lawyer'},
-    {id: 5, name: 'Kevin',},
-];
-
